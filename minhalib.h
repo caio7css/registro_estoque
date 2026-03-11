a14d7190bea2022f2b9b2a4e5f482fc7b0c277b7
Sarah-OAlbuquerque7 <saraho.albuquerque7@gmail.com>
2026-03-11 10:07:34 -0300

    nomes das funções

---
+++ minhalib.h
@@ -0,0 +1,18 @@
+// #ifndef MINHALIB_H
+// #define MINHALIB_H
+
+// No* criarProduto(Produto p);
+// void adicionarProduto(No* cabeca);
+// void listarProdutos(No* cabeca);
+// void buscarProduto(No* cabeca);
+// void editarProduto(No* cabeca);
+// void removerProduto(No** cabeca);
+// void verificarEstoqueBaixo(No* cabeca, int limite);
+// void salvarEmArquivo(No* cabeca, const char* nomeArquivo);
+// void carregarDoArquivo(No* cabeca, const char* nomeArquivo);
+// void liberarLista(No** cabeca); TALVEZ
+// void exibirValorTotal(No* cabeca);
+// void existeCodigo(No* cabeca, int codigo);
+// void limparBuffer();
+
+// #endif
